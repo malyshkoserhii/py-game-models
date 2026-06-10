0a9fa59199890894b6924f4f0573637b5bb951eb
malyshkoserhii <malyshkosergiy@gmail.com>
2026-06-10 21:54:41 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,46 @@
 from django.db import models
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return (f"Race name is: {self.name}, "
+                f"description is: {self.description}")
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return (f"Skill name is: {self.name}, "
+                f"bonus is: {self.bonus}")
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f"Guild name is: {self.name}"
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=False)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    guild = models.ForeignKey(
+        Guild,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return (f"Player nickname is: {self.nickname}, "
+                f"race is: {self.race}"
+                f"guild is: {self.guild if self.guild else 'None'}")
--- main.py
+++ main.py
@@ -1,10 +1,44 @@
 import init_django_orm  # noqa: F401
+import json
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("./players.json", "r") as players_file:
+        data = json.load(players_file)
+
+    for player_name, player_info in data.items():
+        race = player_info["race"]
+
+        race_obj, _ = Race.objects.get_or_create(
+            name=race["name"],
+            description=race["description"]
+        )
+
+        for skill in race.get("skills", []):
+            Skill.objects.get_or_create(
+                name=skill["name"],
+                bonus=skill["bonus"],
+                race=race_obj
+            )
+
+        guild_obj = None
+        guild = player_info.get("guild")
+
+        if guild:
+            guild_obj, _ = Guild.objects.get_or_create(
+                name=guild["name"],
+                description=guild.get("description")
+            )
+
+        Player.objects.get_or_create(
+            nickname=player_name,
+            race=race_obj,
+            guild=guild_obj,
+            bio=player_info.get("bio"),
+            email=player_info.get("email")
+        )
 
 
 if __name__ == "__main__":
